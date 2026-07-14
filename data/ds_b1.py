3fe4fd883f024ff9648da1e4cdcc473f6e25108f
Razvan <146588815+rfvasile@users.noreply.github.com>
2026-07-14 18:33:06 +0200

    Init. data/datamount

---
+++ data/ds_b1.py
@@ -0,0 +1,14 @@
+class CustomDataset:
+    pass
+
+
+def tr_collate_fn():
+    pass
+
+
+def val_collate_fn():
+    pass
+
+
+def batch_to_device():
+    pass
---
+++ datamount/README.md
@@ -0,0 +1,6 @@
+To populate this directory:
+
+``` shell
+# From within the container
+uv run scripts/train_ensemble_gluon.py
+```
